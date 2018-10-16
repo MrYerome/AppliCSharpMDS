c01d62aced7c641e704ba5c70291e002cfe617b4
Vinet <vinet.jerome@gmail.com>
2018-10-16 16:51:20 +0200

    Projet de démarrage final

---
+++ GestionDeClubs/GestionDeClubs.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GestionDeClubs.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
